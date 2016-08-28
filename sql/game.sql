b6f487f4e0b105e07db7f12814e39f22e3a28737
Marco Ammon <ammon.marco@t-online.de>
2016-08-28 18:04:54 +0200

    Added SQL script for "games" table

--- sql/game.sql
+++ sql/game.sql
@@ -16,55 +16,31 @@
  */
 /**
  * Author:  Marco Ammon (Clubfan)
- * Created: 27.08.2016
+ * Created: 28.08.2016
  */
 Begin;
 
-CREATE TABLE /*_*/matches(
+CREATE TABLE /*_*/games(
 --Primary Key
-m_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+m_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES /*_*/matches (m_id) ON DELETE CASCADE ON UPDATE CASCADE,
 --Page ID
 page_id INT UNSIGNED NOT NULL,
 --Date
 m_date TIMESTAMP,
---Participant 1
-participant_1 VARCHAR(255) NOT NULL,
---Participant 2
-participant_2 VARCHAR(255) NOT NULL,
---Participant 1 team template
-p1_template VARCHAR(255),
---Participant 2 team template
-p2_template VARCHAR(255),
 --Participant 1 flag
 p1_flag VARCHAR(31),
 --Participant 2 flag
 p2_flag VARCHAR(31),
---Participant 1 race/heads
-p1_race VARCHAR(31),
---Participant 2 race/heads
-p2_race VARCHAR(31),
---Tournament Page Title
-tournament VARCHAR(255) NOT NULL,
---Liquipedia Tier
-t_tier VARCHAR(63),
---Tournament Name (for ticker etc)
-t_name VARCHAR(255),
---Tournmant icon (File Page)
-t_icon VARCHAR(255),
+--Game length in seconds
+g_length INT,
+--Map
+map VARCHAR(127).
 --Finished
-finished BOOLEAN NOT NULL default FALSE,
---Participant 1 score
-p1_score INT,
---Participant 2 score
-p2_score INT,
+finished BOOLEAN default FALSE,
 --Winner (either 1, 2 or d for draw)
-winner CHAR,
---If walkover, either 1 or 2 as winner
-walkover INT,
---Mode (team, indiv, teamindiv or special modes such as archon or doubles)
-mode VARCHAR(31),
---Additional details as JSON-Object
-details JSON
+g_winner CHAR,
+--Details as JSON-Object (such as hero picks/bans)
+g_details JSON
 )/*wgDBTableOptions*/;
 
 Commit;
