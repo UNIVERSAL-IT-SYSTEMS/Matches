98fb4180f79dcab4daeadc2ce176af6f6155d3fe
Marco Ammon <ammon.marco@t-online.de>
2016-08-30 21:48:22 +0200

    Fixed SQL typos

--- sql/games.sql
+++ sql/games.sql
@@ -1,23 +1,4 @@
-/* 
- * Copyright (C) 2016 Marco Ammon (Clubfan)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * Author:  Marco Ammon (Clubfan)
- * Created: 28.08.2016
- */
+
 Begin;
 
 CREATE TABLE /*_*/games(
@@ -47,7 +28,7 @@ g_winner CHAR,
 mode VARCHAR(31),
 --Details as JSON-Object (such as hero picks/bans)
 g_details JSON
-)/*wgDBTableOptions*/;
+)/*$wgDBTableOptions*/;
 
 --Automatically genereated for foreign keys
 --CREATE INDEX /*_*//*i*/games_m_id ON /*_*/games (m_id);
--- sql/matches.sql
+++ sql/matches.sql
@@ -1,23 +1,3 @@
-/* 
- * Copyright (C) 2016 Marco Ammon (Clubfan)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * Author:  Marco Ammon (Clubfan)
- * Created: 27.08.2016
- */
 Begin;
 
 CREATE TABLE /*_*/matches(
@@ -65,7 +45,7 @@ walkover INT,
 mode VARCHAR(31),
 --Additional details as JSON-Object
 details JSON
-)/*wgDBTableOptions*/;
+)/*$wgDBTableOptions*/;
 
 CREATE INDEX /*_*//*i*/matches_p1 ON /*_*/matches (participant_1);
 CREATE INDEX /*_*//*i*/matches_p2 ON /*_*/matches (participant_2);
